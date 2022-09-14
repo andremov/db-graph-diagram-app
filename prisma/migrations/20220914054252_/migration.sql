b39a376eb93715ea0a09c52d51cab9d4769115d1
demarchenac <demarchenac@uninorte.edu.co>
2022-09-14 00:44:24 -0500

    Chore: Testing migrations

---
+++ prisma/migrations/20220914054252_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `message` on the `Example` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Example" DROP COLUMN "message";
